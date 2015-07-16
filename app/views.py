892dc1e753155dbe791f5f2e366b564c726a9a89
milo zhao <milo.zhao@qq.com>
2015-07-17 07:29:03 +0800

    add templates

--- app/views.py
+++ app/views.py
@@ -2,9 +2,19 @@
 # coding = utf-8
 __author__ = "milo zhao"
 
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index/")
 def index():
-	return "Hello Flask!!!"
+	user = {"nickname":"milo"}
+	posts = [
+			{"author":{"nickname":"Ancel"},
+			 "body":"This is my first Flask project"
+			 	},
+			{"author":{"nickname":"Andy"},
+			 "body":"This is my micro blog"
+				}
+			]
+	return render_template("index.html",title="Flask",user=user,posts=posts)
